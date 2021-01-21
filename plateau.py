13ce1cc815e01ebfb68c1a7eeb243860ac89928b
ahlem <ahlem.romdhane@le-campus-numerique.fr>
2021-01-21 15:24:48 +0100

    Plus de fonctions

--- plateau.py
+++ plateau.py
@@ -13,15 +13,32 @@ class Plateau:
         self.liste[4][4]=1
         return self
 
+
+    def add_pion(self,x,y,couleur):
+        self.liste[x][y]=couleur
+        return self
+
+
+    def add_blanc(self, x,y):
+        self.add_pion(x,y,1)
+        return self
     
+    def add_noir(self, x,y):
+        self.add_pion(x,y,0)
+        return self
+
+    def flip(self, x, y):
+        self.liste[x][y]=abs(self.liste[x][y]-1)
+        return self
 
 
-
-
-
-
-#l=Plateau(8)
+l=Plateau(8)
  
-#print(l, "\n")
-#print(l.initialisation(), "\n")
+print(l, "\n")
+print(l.initialisation(), "\n")
 #print(l)
+
+print(l.add_blanc(3,2), "\n")
+print(l.add_noir(4,2), "\n")
+
+print(l.flip(3,2),"\n")
